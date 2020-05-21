f9ec73284c80dc1ee7ec292a355a684e290ae696
choongyul <choongyul2@gmail.com>
2020-05-22 07:14:51 +0900

    solve problem

--- programmers/src/main/java/com/choong/problem/programmers/level1/MockExam.java
+++ programmers/src/main/java/com/choong/problem/programmers/level1/MockExam.java
@@ -1,33 +1,41 @@
 package com.choong.problem.programmers.level1;
 
+import java.util.stream.IntStream;
+
+/**
+ * 
+ * @title	: 모이고사
+ * @author	: CHOONG
+ * @Desc	: 프로그래머스 모이고사 문제 
+ *
+ */
+
 public class MockExam {
 
-  public static int[] solution(int[] answers) {
+  public int[] solution(int[] answers) {
     final int[] firstAnswer = {1, 2, 3, 4, 5};
     final int[] secondAnswer = {2, 1, 2, 3, 2, 4, 2, 5};
     final int[] thirdAnswer = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
     int[] answer = {};
+    int[] scores = {0, 0, 0, 0};
+
     for (int i = 0; i < answers.length; i++) {
       if (answers[i] == firstAnswer[i % 5]) {
-
+        scores[1]++;
       }
-
       if (answers[i] == secondAnswer[i % 8]) {
-
+        scores[2]++;
       }
-
       if (answers[i] == thirdAnswer[i % 10]) {
-
+        scores[3]++;
       }
-
     }
+
+    int max = IntStream.of(scores).max().getAsInt();
+
+    answer = IntStream.range(1, scores.length).filter(i -> (max == scores[i])).toArray();
+
     return answer;
   }
-
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
-  }
-
 }
