642198e8c3d74af78211659c4536ca9977dcf54e
choongyul <choongyul2@gmail.com>
2020-06-01 06:38:19 +0900

    x만큼 간격이 있는 n개의 숫자 문제

---
+++ programmers/src/main/java/com/choong/problem/programmers/level1/IntervalByX.java
@@ -0,0 +1,22 @@
+package com.choong.problem.programmers.level1;
+
+import java.util.stream.LongStream;
+
+/**
+ * 
+ * @title   : x만큼 간격이 있는 n개의 숫자
+ * @author  : CHOONG
+ * @Desc    : 프로그래머스 x만큼 간격이 있는 n개의 숫자 문
+ *
+ */
+
+public class IntervalByX {
+
+  public long[] solution(int x, int n) {
+    long[] answer = {};
+    answer = LongStream.iterate(x, i -> i + x).limit(n).toArray();
+
+    return answer;
+  }
+
+}
---
+++ programmers/src/test/java/com/choong/problem/programmers/level1/IntervalByXTest.java
@@ -0,0 +1,19 @@
+package com.choong.problem.programmers.level1;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("x만큼 간격이 있는 n개의 숫자")
+class IntervalByXTest {
+
+  @DisplayName("기본 테스트 케이스")
+  @Test
+  void testSolution() {
+    IntervalByX intervalByX = new IntervalByX();
+    assertArrayEquals(new long[] {2,4,6,8,10}, intervalByX.solution(2, 5));
+    assertArrayEquals(new long[] {4,8,12}, intervalByX.solution(4, 3));
+    assertArrayEquals(new long[] {-4,-8}, intervalByX.solution(-4, 2));
+  }
+
+}
