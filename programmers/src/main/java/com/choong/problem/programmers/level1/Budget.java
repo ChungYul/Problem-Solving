e009bf5290c813df4ec5a0950fcbb6e05491ae3a
choongyul <choongyul2@gmail.com>
2020-06-01 05:58:41 +0900

    fix typo

--- programmers/src/main/java/com/choong/problem/programmers/level1/Budget.java
+++ programmers/src/main/java/com/choong/problem/programmers/level1/Budget.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Budget {
 
-  public static int solution(int[] d, int budget) {
+  public int solution(int[] d, int budget) {
     int answer = 0;
     Arrays.sort(d);
     for(int val : d) {
@@ -16,14 +16,6 @@ public class Budget {
       }
     }
     return answer;
-  }
-
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    int answer = Budget.solution(new int[] {1,3,2,5,4}, 9);
-    System.out.println("exam1 : " + answer);
-    answer = Budget.solution(new int[] {2,2,3,3}, 10);
-    System.out.println("exam2 : " + answer);
   }
 
 }
