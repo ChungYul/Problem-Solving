8c5c80d779c0c129214cd1e1ae57e8ea3a405f33
choongyul2 <choongyul2@gmail.com>
2020-06-05 09:04:41 +0900

    fix error

--- programmers/src/test/java/com/choong/problem/programmers/level2/SortFileNamesTest.java
+++ programmers/src/test/java/com/choong/problem/programmers/level2/SortFileNamesTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 @DisplayName("파일명 정렬")
 class SortFileNamesTest {
 
@@ -12,13 +13,12 @@ class SortFileNamesTest {
   void testSolution() {
     SortFileNames sortFileNames = new SortFileNames();
     assertArrayEquals(
-        new String[] {"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II",
-            "F-14 Tomcat"},
+        new String[] {"A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"},
         sortFileNames.solution(new String[] {"F-5 Freedom Fighter", "B-50 Superfortress",
             "A-10 Thunderbolt II", "F-14 Tomcat"}));
     assertArrayEquals(
-        new String[] {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"},
-        sortFileNames.solution(new String[] {"img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"}));
+        new String[] {"img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"},
+        sortFileNames.solution(new String[] {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"}));
   }
 
 }
