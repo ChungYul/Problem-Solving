be9d147ac2ae41fdbdec0ff14b3a92e546b83fde
choongyul <choongyul2@gmail.com>
2020-07-10 05:23:36 +0900

    코드 정렬

--- programmers/src/test/java/com/choong/problem/programmers/level2/DisguiseTest.java
+++ programmers/src/test/java/com/choong/problem/programmers/level2/DisguiseTest.java
@@ -11,20 +11,20 @@ class DisguiseTest {
   @DisplayName("기본 테스트 케이스")
   void testSolution_with_basic_testcase() {
     Disguise disguise = new Disguise();
-    
-   assertEquals(5, disguise.solution(new String[][] {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"},
-        {"green_turban", "headgear"}}));
-    assertEquals(3, disguise.solution(new String[][] {{"crow_mask", "face"}, {"blue_sunglasses", "face"},
-        {"smoky_makeup", "face"}}));    
+
+    assertEquals(5, disguise.solution(new String[][] {{"yellow_hat", "headgear"},
+        {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+    assertEquals(3, disguise.solution(new String[][] {{"crow_mask", "face"},
+        {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
   }
-  
+
   @Test
   @DisplayName("Edge 테스트 케이스")
   void testSolution_with_edge_testcase() {
     Disguise disguise = new Disguise();
-    
-    assertEquals(35, disguise.solution(new String[][] {{"1", "face"}, {"2", "face"}, {"3", "face"}, {"4", "hair"},
-      {"5", "hair"}, {"6", "arm"}, {"7", "arm"}}));
+
+    assertEquals(35, disguise.solution(new String[][] {{"1", "face"}, {"2", "face"}, {"3", "face"},
+        {"4", "hair"}, {"5", "hair"}, {"6", "arm"}, {"7", "arm"}}));
   }
 
 }
